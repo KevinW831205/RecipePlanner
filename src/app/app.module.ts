1f9ebbf6b75a63e5653a3440d9b655d619ce8d05
Kevin Wang <bird831205@hotmail.com>
2020-01-17 12:59:22 -0500

    creating a recipe-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipeCardComponent } from './recipe-card/recipe-card.component';
 import { RecipeStarRatingComponent } from './recipe-star-rating/recipe-star-rating.component';
+import { RecipePageComponent } from './recipe-page/recipe-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipeCardComponent,
     RecipeStarRatingComponent,
+    RecipePageComponent,
   ],
   imports: [
     NgbModule,
