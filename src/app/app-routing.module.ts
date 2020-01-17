68ba6d6c104799135753cb06d4c2ae9a2842952b
Kevin Wang <bird831205@hotmail.com>
2020-01-17 13:03:43 -0500

    some routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipePageComponent } from './recipe-page/recipe-page.component';
 
+const routes: Routes = [
 
-const routes: Routes = [];
+  { path: 'recipes', component: RecipePageComponent },
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
