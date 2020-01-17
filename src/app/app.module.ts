d5769adabfc8e265df0b167038332e8b82b9a365
Kevin Wang <bird831205@hotmail.com>
2020-01-17 12:28:35 -0500

    installing bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeCardComponent } from './recipe-card/recipe-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeCardComponent,
   ],
   imports: [
+    NgbModule,
     BrowserModule,
     AppRoutingModule
   ],
