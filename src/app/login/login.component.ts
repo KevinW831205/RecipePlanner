7cb2d59d45b9d790b55516ef323e3ce721873bb0
KevinW <bird831205@hotmail.com>
2020-01-17 19:12:42 -0500

    login button can close modal

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public activeModal: NgbActiveModal) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    setTimeout(() => { this.activeModal.close('Close click') }, 1000)
   }
 
 }
