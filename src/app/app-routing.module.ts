e91c85b252eb0f88b80ebf834da4a57a2156c7c3
KevinW <bird831205@hotmail.com>
2020-01-24 14:56:40 -0500

    creating a data service to use

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { RecipePageComponent } from './recipe-page/recipe-page.component';
 import { ProfileComponent } from './profile/profile.component';
 import { RecipeDetailPageComponent } from './recipe-detail-page/recipe-detail-page.component';
+import { CategoryTagsPageComponent } from './admin/category-tags-page/category-tags-page.component';
 
 const routes: Routes = [
 
   { path: 'recipes', component: RecipePageComponent },
   { path: 'profile/:username', component: ProfileComponent },
   { path: 'recipe/:id', component: RecipeDetailPageComponent },
+  { path: 'admin/category-tags', component: CategoryTagsPageComponent },
+  // { path:'', component: },
   { path: '', redirectTo: '/recipes', pathMatch: 'full' }
 ];
 
---
+++ src/app/service/data-service.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataServiceService {
+
+  constructor(private http: HttpClient, private url) { }
+
+  getAll() {
+    try {
+      return this.http.get(this.url);
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
+
+  get(id) {
+    try {
+      return this.http.get(this.url + '/' + id);
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
+
+
+  create(resource) {
+    try {
+      return this.http.post(this.url, JSON.stringify(resource));
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
+
+  update(resource) {
+    try {
+      this.http.patch(this.url + "/" + resource.id, JSON.stringify(resource));
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
+
+  delete(id) {
+    try {
+      return this.http.delete(this.url + "/" + id);
+    } catch (error) {
+      this.handleError(error);
+    }
+
+  }
+
+  private handleError(error: Response) {
+    if (error.status === 400) {
+    }
+    if (error.status === 404) {
+    }
+  }
+
+}
