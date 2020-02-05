1c5c6192bd71a9a98354079b3004d31623facd79
KevinW <bird831205@hotmail.com>
2020-02-05 08:29:38 -0500

    some navbar logged in work

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,9 +10,13 @@ export class NavbarComponent implements OnInit {
 
   user;
 
-  constructor() { }
+  constructor(private authService : AuthService) { }
 
   ngOnInit() {
+  }
+
+  logout(){
+
   }
 
 }
