d245e0dc1bd915fe504e1fb7133e505c63606f74
Kevin Wang <bird831205@hotmail.com>
2020-02-05 09:11:25 -0500

    some redirecting in the login service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { httpOptions, baseUrl } from './httpConfig';
 import { SignupInfo } from '../models/SignupInfo';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,16 @@ import { SignupInfo } from '../models/SignupInfo';
 export class AuthService {
   user$: Observable<Account>;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) { }
 
-  login(loginInfo : SignupInfo): Observable<Account>{
-    return this.http.post<Account>(baseUrl+"login/",loginInfo,httpOptions);
+  login(loginInfo : SignupInfo){
+    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '.'
+    this.user$ =this.http.post<Account>(baseUrl+"login/",loginInfo,httpOptions);
+    this.router.navigate([returnUrl])
   }
 
   logout(){
-    
+    this.user$ = null;
+    this.router.navigate(['/'])
   }
 }
