abaf176774fe69e9ef452a4dfea988ad0a5168e9
Kevin Wang <bird831205@hotmail.com>
2020-02-07 08:57:28 -0500

    wrapup

--- src/app/edit-recipe-page/edit-recipe-page.component.ts
+++ src/app/edit-recipe-page/edit-recipe-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RecipeService } from '../services/recipe.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-edit-recipe-page',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditRecipePageComponent implements OnInit {
 
-  constructor() { }
+  authorized = false;
+
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService, private authService: AuthService) { }
 
   ngOnInit() {
+    let recipeId = this.route.snapshot.paramMap.get('id');
+    this.recipeService.get
+
   }
 
 }
