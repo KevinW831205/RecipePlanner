80c274cb9c4cd37299751be0b9b2137b9e07eddf
Kevin Wang <bird831205@hotmail.com>
2020-02-10 10:10:37 -0500

    resetting ingredient form

--- src/app/ingredients/ingredients.component.ts
+++ src/app/ingredients/ingredients.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../models/Recipe';
 import { IngredientService } from '../services/ingredient.service';
 import { Ingredient } from '../models/Ingredient';
+import { FormBuilder, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-ingredients',
@@ -12,22 +13,24 @@ export class IngredientsComponent {
   @Input() recipe: Recipe;
   @Input('canEdit') canEdit: boolean = true;
   edit: boolean[] = [];
+
   ingredientInput: Partial<Ingredient> = {
     name: "",
     amount: ""
   }
 
-  constructor(private ingredientService: IngredientService, ) { }
+  constructor(private ingredientService: IngredientService) { }
 
-  addIngredient() {
+  addIngredient(form: NgForm) {
     let ingredient = new Ingredient(this.ingredientInput);
     ingredient.recipeId = this.recipe.id;
 
     this.ingredientService.create(ingredient).subscribe(
       res => {
         this.recipe.ingredientList.push(res);
-        this.ingredientInput.name = "";
-        this.ingredientInput.amount = "";
+        // this.ingredientInput.name = "";
+        // this.ingredientInput.amount = "";
+        form.reset();
       },
       err => {
         console.log(err);
