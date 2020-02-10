6812b947f2ca3ed2020cc49e9d266de3e4a71bf5
KevinW <bird831205@hotmail.com>
2020-02-10 08:16:36 -0500

    debug

--- src/app/models/Ingredient.ts
+++ src/app/models/Ingredient.ts
@@ -1,9 +1,9 @@
 export class Ingredient {
 
-    id: number,
-    recipeId: number,
-    name: string,
-    amount: string
+    id: number;
+    recipeId: number;
+    name: string;
+    amount: string;
 
     constructor(init?: Partial<Ingredient>) {
         Object.assign(this, init)
