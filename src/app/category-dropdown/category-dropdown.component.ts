de7fa232da6a65430ebfa441fa7aec1be235d20c
Kevin Wang <bird831205@hotmail.com>
2020-02-12 14:03:02 -0500

    dropdown category selction

--- src/app/category-dropdown/category-dropdown.component.ts
+++ src/app/category-dropdown/category-dropdown.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../services/category.service';
+import { Observable } from 'rxjs';
+import { Category } from '../models/Category';
 
 @Component({
   selector: 'app-category-dropdown',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryDropdownComponent implements OnInit {
 
-  constructor() { }
+  categories$: Observable<Category[]>;
+  currentCategory = "All";
+
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit() {
+    this.categories$ = this.categoryService.getAll();
+  }
+
+  setCurrentCategory(val: string) {
+    this.currentCategory = val;
   }
 
 }
