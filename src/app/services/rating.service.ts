45fd14c3cc6f0ef87d8e755352164f7cddc7070c
KevinW <bird831205@hotmail.com>
2020-02-13 15:09:58 -0500

    refactor rating service

--- src/app/services/rating.service.ts
+++ src/app/services/rating.service.ts
@@ -3,19 +3,25 @@ import { DataService } from './data.service';
 import { Rating } from '../models/Rating';
 import { baseUrl } from './httpConfig';
 import { HttpClient } from '@angular/common/http';
+import { ErrorService } from './error.service';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RatingService extends DataService<Rating>{
 
-  constructor(http: HttpClient) {
-    super(http, baseUrl + "rating")
+  constructor(http: HttpClient, errorService: ErrorService) {
+    super(http, baseUrl + "rating", errorService)
   }
 
   checkRated(recipeId, accountId): Promise<Rating> {
     try {
-      return this.http.get<Rating>(`${baseUrl}rating/rated?recipe_id=${recipeId}&account_id=${accountId}`).toPromise();
+      return this.http.get<Rating>(`${baseUrl}rating/rated?recipe_id=${recipeId}&account_id=${accountId}`).pipe(
+        catchError(err => {
+          return super.handleError(err);
+        })
+      ).toPromise();
     } catch (error) {
       console.log(error)
       return null;
