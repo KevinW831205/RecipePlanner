b796178a498efcbbe7f8c6e9d6dc6ba51d3b6d1a
KevinW <bird831205@hotmail.com>
2020-02-13 15:15:53 -0500

    recipeService refactor

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -19,64 +19,43 @@ export class RecipeService extends DataService<Recipe> {
 
   getPublished(): Observable<Recipe[]> {
     return this.http.get<Recipe[]>(baseUrl + "recipe/published", httpOptions).pipe(
-      catchError(err => { return super.handleError(err) }
-      )
+      catchError(err => { return super.handleError(err) })
     );
-
-
-    // try {
-    // return this.http.get<Recipe[]>(baseUrl + "recipe/published", httpOptions);
-    // } catch (error) {
-    //   super.handleError(error);
-    // }
   }
 
   patchTogglePublished(recipeId): Observable<Recipe> {
-    try {
-      return this.http.patch<Recipe>(baseUrl + "recipe/published/" + recipeId, null, httpOptions);
-    } catch (error) {
-      super.handleError(error);
-    }
+    return this.http.patch<Recipe>(baseUrl + "recipe/published/" + recipeId, null, httpOptions).pipe(
+      catchError(err => { return super.handleError(err) })
+    );
   }
 
   patchName(recipeId, name): Observable<Recipe> {
-    try {
-      return this.http.patch<Recipe>(`${baseUrl}recipe/name/${recipeId}?name=${name}`, null, httpOptions)
-    } catch (error) {
-      super.handleError(error)
-    }
+    return this.http.patch<Recipe>(`${baseUrl}recipe/name/${recipeId}?name=${name}`, null, httpOptions).pipe(
+      catchError(err => { return super.handleError(err) })
+    );
   }
 
   patchImageUrl(recipeId, url): Observable<Recipe> {
-    try {
-      return this.http.patch<Recipe>(`${baseUrl}recipe/imageurl/${recipeId}?url=${url}`, null, httpOptions);
-    } catch (error) {
-      super.handleError(error)
-    }
+    return this.http.patch<Recipe>(`${baseUrl}recipe/imageurl/${recipeId}?url=${url}`, null, httpOptions).pipe(
+      catchError(err => { return super.handleError(err) })
+    )
   }
 
   patchDescription(recipeId, description: string): Observable<Recipe> {
-    try {
-      return this.http.patch<Recipe>(`${baseUrl}recipe/description/${recipeId}`, description, httpOptions);
-    } catch (error) {
-      super.handleError(error);
-    }
+    return this.http.patch<Recipe>(`${baseUrl}recipe/description/${recipeId}`, description, httpOptions).pipe(
+      catchError(err => { return super.handleError(err) })
+    )
   }
 
   addTag(recipeId, tagId) {
-    try {
-      return this.http.put<Recipe>(`${baseUrl}recipe/tag?recipe_id=${recipeId}&tag_id=${tagId}`, null, httpOptions);
-    } catch (error) {
-      super.handleError(error);
-    }
+    return this.http.put<Recipe>(`${baseUrl}recipe/tag?recipe_id=${recipeId}&tag_id=${tagId}`, null, httpOptions).pipe(
+      catchError(err => { return super.handleError(err) })
+    );
   }
 
   deleteTag(recipeId, tagId) {
-    try {
-      return this.http.delete<Recipe>(`${baseUrl}recipe/tag?recipe_id=${recipeId}&tag_id=${tagId}`, httpOptions);
-    } catch (error) {
-      super.handleError(error);
-    }
+    return this.http.delete<Recipe>(`${baseUrl}recipe/tag?recipe_id=${recipeId}&tag_id=${tagId}`, httpOptions).pipe(
+      catchError(err => { return super.handleError(err) })
+    );
   }
-
 }
